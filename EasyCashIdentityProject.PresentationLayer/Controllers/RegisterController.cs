e7a5edb94b40abb51f446f7652bae3af5c96441c
senaboyuktas <senabykts99@gmail.com>
2023-05-11 22:09:19 +0300

    11. ders sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -30,12 +30,22 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     UserName = appUserRegisterDto.UserName,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
-                    Email = appUserRegisterDto.Email
-                };
+                    Email = appUserRegisterDto.Email,
+                    City = "aaa",
+                    District = "bbb",
+                    ImageUrl = "ccc"
+				};
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded) 
                 {
                     return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
                 }
             }
             return View();
