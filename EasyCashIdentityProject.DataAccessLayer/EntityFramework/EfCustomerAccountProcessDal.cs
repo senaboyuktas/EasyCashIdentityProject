a47c87225881f82787600a955f887a7848426f77
senaboyuktas <senabykts99@gmail.com>
2024-02-07 15:12:21 +0300

    30. ders sonu

--- EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
+++ EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
@@ -1,6 +1,8 @@
 ﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using EasyCashIdentityProject.DataAccessLayer.Repositories;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,11 @@ namespace EasyCashIdentityProject.DataAccessLayer.EntityFramework
 {
     public class EfCustomerAccountProcessDal : GenericRepository<CustomerAccountProcess>, ICustomerAccountProcessDal
     {
+        public List<CustomerAccountProcess> MyLastProcess(int id)
+        {
+            using var context = new Context();
+            var values = context.CustomerAccountProcesses.Include(y => y.SenderCustomer).Include(w => w.ReceiverCustomer).ThenInclude(z => z.AppUser).Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
+            return values;
+        }
     }
 }
